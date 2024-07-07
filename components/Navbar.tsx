e92ab30d291d630e139e6cd581956aab7ab8293b
Zakaria Elqarch <zakariaelqarch@gmail.com>
2024-07-07 14:59:55 +0100

    navbar added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "Search" },
+  { src: "/assets/icons/black-heart.svg", alt: "Heart" },
+  { src: "/assets/icons/user.svg", alt: "User" },
+];
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="Logo"
+          />
+          <p className="nav-logo">
+            Price <span className="text-primary">Wise</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              width={28}
+              height={28}
+              alt={icon.alt}
+              className="object-contain"
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
