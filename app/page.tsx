ee04eb6f8f80f5aa265737a8ea348f41612bed3d
Zakaria Elqarch <zakariaelqarch@gmail.com>
2024-07-08 17:56:41 +0100

    product detail page

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@ import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 import React from "react";
-
-const Home = () => {
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
+const Home = async () => {
+  const allProducts = await getAllProducts();
   return (
     <>
       <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
-          <div className="flex flex-col justify-center"> 
+          <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping Starts Here:
-              <Image 
+              <Image
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
                 width={16}
@@ -25,7 +27,8 @@ const Home = () => {
             </h1>
 
             <p className="mt-6">
-              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
             </p>
 
             <Searchbar />
@@ -37,8 +40,8 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {['Apple Ihone 15','Book', 'Sneakers'].map((product)=>(
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -40,3 +40,44 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
 }
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) return null;
+    return product;
+  } catch (error: any) {
+    console.log("Get product by Id error: ", error);
+  }
+}
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+    return products;
+  } catch (error) {
+    console.log("Get all products error: ", error);
+  }
+}
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+    if (!currentProduct) return null;
+
+    const similarProduct = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProduct;
+  } catch (error) {
+    console.log("Get all products error: ", error);
+  }
+}
