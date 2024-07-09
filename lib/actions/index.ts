e297dfcd42304e694462dab4d36d44aac76352b6
z.elqarch <zakariaelqarch@gmail.com>
2024-07-09 14:10:16 +0100

    Mailer set

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,34 +1,40 @@
-"use server";
+"use server"
 
-import { User } from "@/types";
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
-import { revalidatePath } from "next/cache";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
+
 export async function scrapeAndStoreProduct(productUrl: string) {
-  if (!productUrl) return;
+  if(!productUrl) return;
 
   try {
     connectToDB();
+
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
-    if (!scrapedProduct) return;
+
+    if(!scrapedProduct) return;
 
     let product = scrapedProduct;
 
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
-    if (existingProduct) {
+
+    if(existingProduct) {
       const updatedPriceHistory: any = [
         ...existingProduct.priceHistory,
-        { price: scrapedProduct.currentPrice },
-      ];
+        { price: scrapedProduct.currentPrice }
+      ]
+
       product = {
         ...scrapedProduct,
         priceHistory: updatedPriceHistory,
         lowestPrice: getLowestPrice(updatedPriceHistory),
         highestPrice: getHighestPrice(updatedPriceHistory),
         averagePrice: getAveragePrice(updatedPriceHistory),
-      };
+      }
     }
 
     const newProduct = await Product.findOneAndUpdate(
@@ -36,9 +42,10 @@ export async function scrapeAndStoreProduct(productUrl: string) {
       product,
       { upsert: true, new: true }
     );
+
     revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
-    throw new Error(`Failed to create/update product: ${error.message}`);
+    throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
 
@@ -48,10 +55,11 @@ export async function getProductById(productId: string) {
 
     const product = await Product.findOne({ _id: productId });
 
-    if (!product) return null;
+    if(!product) return null;
+
     return product;
-  } catch (error: any) {
-    console.log("Get product by Id error: ", error);
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -60,9 +68,10 @@ export async function getAllProducts() {
     connectToDB();
 
     const products = await Product.find();
+
     return products;
   } catch (error) {
-    console.log("Get all products error: ", error);
+    console.log(error);
   }
 }
 
@@ -71,36 +80,37 @@ export async function getSimilarProducts(productId: string) {
     connectToDB();
 
     const currentProduct = await Product.findById(productId);
-    if (!currentProduct) return null;
 
-    const similarProduct = await Product.find({
+    if(!currentProduct) return null;
+
+    const similarProducts = await Product.find({
       _id: { $ne: productId },
     }).limit(3);
 
-    return similarProduct;
+    return similarProducts;
   } catch (error) {
-    console.log("Get all products error: ", error);
+    console.log(error);
   }
 }
 
-export async function addUserEmailToProduct(
-  productId: string,
-  userEmail: string
-) {
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
   try {
     const product = await Product.findById(productId);
-    if (!product) return;
 
-    const userExists = product.users.some(
-      (user: User) => user.email === userEmail
-    );
-    if (!userExists) {
+    if(!product) return;
+
+    const userExists = product.users.some((user: User) => user.email === userEmail);
+
+    if(!userExists) {
       product.users.push({ email: userEmail });
+
       await product.save();
 
-      // const emailContent = generateEmailBody(product, "WELCOME");
+      const emailContent = await generateEmailBody(product, "WELCOME");
+
+      await sendEmail(emailContent, [userEmail]);
     }
   } catch (error) {
-    console.log("Add user to product erro: ", error);
+    console.log(error);
   }
 }
